e16fd02c2272aff995874de797666167c974c4ab
Benz <benzhomeme@gmail.com>
2023-09-01 18:10:03 +0700

    feat: add HomepagePhoto and HomePagenavbar

---
+++ client/src/pages/HomePages.jsx
@@ -0,0 +1,13 @@
+import Navbar from "/src/components/Homepage/Navbar.jsx"
+
+function Homepage () {
+
+  return (
+    <div className="prompt">
+      <Navbar />
+    </div>
+    
+  )
+}
+
+export default Navbar;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
