e20df67f07fe3cd253a4d5f8afb5ba85b118224b
Benz <benzhomeme@gmail.com>
2023-09-02 19:51:40 +0700

    feat: add Sogan , TopServices ,and ForJob sections

--- client/src/components/HomePage/Navbar.jsx
+++ client/src/components/HomePage/Navbar.jsx
@@ -4,11 +4,12 @@ function Navbar () {
 
   return(
     
-      <div className="flex justify-between p-5 shadow-md">
+      <div className="flex justify-between p-5 shadow-[2px_2px_24px_rgba(23,51,106,0.12)] sticky top-0 z-[100] bg-white">
         <div className="flex ml-40">
         <HouseIcon />
-        <div className="text-xl font-semibold text-[#336DF2] prompt">HomeServices</div>
-        <div className="center-text text-sm font-bold prompt ml-10 p-1">บริการของเรา</div>
+        <div className="text-xl font-semibold text-[#336DF2] prompt ml-2">HomeServices</div>
+        <div className="center-text text-sm font-bold prompt ml-10 p-2">
+          <button className="cursor-pointer">บริการของเรา</button></div>
         </div>
 
         <div>
