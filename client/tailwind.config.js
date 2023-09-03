458f07c1665b04e4be27cdbf5671fd1ce86d1e82
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-09-03 16:48:19 +0700

    fix:fix code in tailwind.config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
      /*blue*/
@@ -48,8 +51,7 @@ export default {
     },
     extend: {}
   },
-  plugins: [require('daisyui')],
-};
-
+  plugins: [require("daisyui")],
+}
 
 
