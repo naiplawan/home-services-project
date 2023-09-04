b253547ff7f96a07ce773c8bc1765b412979d549
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-09-04 19:09:07 +0700

    feat:add form

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,29 @@
-function Navbar(){
-    return (
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-  </div>
-</div>
-    )
+import HouseIcon from '../assets/HouseIcon.svg';
+
+function Navbar () {
+
+  return(
+    
+      <div className="flex justify-between p-5 shadow-[2px_2px_24px_rgba(23,51,106,0.12)] sticky top-0 z-[100] bg-white">
+        <div className="flex lg:ml-40 items-center">
+        <img src={HouseIcon} alt=""/>
+        <div className="text-xl font-semibold text-[#336DF2] prompt ml-2 ">
+          HomeServices
+          </div>
+        <div className="items-center text-sm font-bold prompt lg:ml-10 px-3 pt-1.5">
+          <button className="cursor-pointer">
+            บริการของเรา
+            </button>
+          </div>
+        </div>
+
+        <div className="lg:mr-40">
+          <button className="prompt text-sm font-bold text-[#336DF2] border border-[#336DF2] w-[100px] h-[35px] rounded-lg ">เข้าสู่ระบบ</button>
+        </div>
+      </div>
+    
+    
+  )
 }
 
 export default Navbar;
