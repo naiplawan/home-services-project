6e8fe3704649425d0f2d16ba5f2d5e5f6b570c80
Baitoey <you@example.com>
2023-09-04 10:04:45 +0700

    feat: add Login Page

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,11 +1,9 @@
 import '../styles/App.css'
 import { Button } from 'antd';
-import { useForm } from "react-hook-form";
+import LoginForm from './LoginPage';
 
 function App() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
 
   return (
     <>
@@ -13,21 +11,7 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <div></div>
-    {/* Ant Design */}
-    <Button type="primary">Button</Button>
-
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+    <LoginForm/>
 
     </>
   )
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,80 @@
+import { Button, Checkbox, Form, Input } from "antd";
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+const LoginForm = () => (
+  <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <div>
+      <h1>เข้าสู่ระบบ</h1>
+    </div>
+
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: "Please input your username!",
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: "Please input your password!",
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+export default LoginForm;
