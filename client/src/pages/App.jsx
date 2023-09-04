c86e06a0e41ff26c5e9748100531d6bc27a364c7
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-09-04 23:50:25 +0700

    feat:add form

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,12 +1,10 @@
 import RegisterPage from "./RegisterPage";
 import "../styles/App.css";
 import { Button } from "antd";
-import Navbar from "../components/Navbar";
 
 function App() {
   return (
     <>
-    <Navbar/>
       <div className="text-10xl font-bold underline ">Hello world!</div>
       <div></div>
       <Button type="primary">Button</Button>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,6 @@
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Form, Input, Checkbox } from "antd";
+import Navbar from "../components/Navbar";
+
 const layout = {
   labelCol: {
     span: 10,
@@ -8,73 +10,167 @@ const layout = {
   },
 };
 
-
-/* eslint-disable no-template-curly-in-string */
 const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      email: '${label} is not a valid email!',
-      number: '${label} is not a valid number!',
-    },
-    number: {
-      range: '${label} must be between ${min} and ${max}',
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
-  
-  function onFinish(values) {
-    console.log(values);
-  }
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    phonenumber: "${label} is not a valid phone number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
 
-  
+function onFinish(values) {
+  console.log(values);
+}
+
 function RegisterPage() {
   return (
-    <div className="flex content-center items-center">
-      <div className='text-2xl text-blue-950'> ลงทะเบียน </div>
-      <Form
-        {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        style={{ maxWidth: 600 }}
-        validateMessages={validateMessages}
-      >
-        <Form.Item
-          name={["user", "name"]}
-          label="ชื่อ-นามสกุล"
-          placeholder="กรุณากรอกชื่อ นามสกุล"
-          rules={[{ required: true }]}
-        >
-          <Input />
-        </Form.Item>
+    <div className="register-form-container">
+      <Navbar />
+      <div className="flex flex-col justify-center items-center bg-bg ">
+        <h1 className="text-blue950 text-center text-[32px] font-medium">
+          ลงทะเบียน
+        </h1>
 
-        <Form.Item
-          name={["user", "phonenumber"]}
-          label="เบอร์โทรศัพท์"
-          rules={[{ type: "phonenumber", min: 0, max: 10 },{ required: true }]}
+        <Form
+          {...layout}
+          name="nest-messages"
+          onFinish={onFinish}
+          style={{ maxWidth: 600 }}
+          validateMessages={validateMessages}
         >
-          <InputNumber />
-        </Form.Item>
+          <Form.Item
+            className="mt-5 "
+            name={["user", "name"]}
+            label="ชื่อ-นามสกุล"
+            rules={[
+              {
+                required: true,
+                message: "กรุณากรอกชื่อ-นามสกุล",
+              },
+              {
+                pattern: /^[-a-zA-Z'.]+ [-a-zA-Z'.]+$/,
+                message: "กรุณากรอกชื่อ นามสกุลให้ถูกต้อง",
+              },
+            ]}
+          >
+            <Input
+              className="border rounded-lg border-grey300 w-full h-11 px-4 py-2.5"
+              placeholder="กรุณากรอกชื่อ - นามสกุล"
+            />
+          </Form.Item>
 
-        <Form.Item
-          name={["user", "email"]}
-          label="Email"
-          rules={[{ type: "email" }]}
-        >
-          <Input />
-        </Form.Item>
-       
-        <Form.Item name={["user", "website"]} label="Website">
-          <Input />
-        </Form.Item>
-        <Form.Item name={["user", "introduction"]} label="Introduction">
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button  type="primary" htmlType="submit">
-            ลงทะเบียน
-          </Button>
-        </Form.Item>
-      </Form>
+          {/* Remove duplicate email field or update its label */}
+
+          <Form.Item
+            className="mt-5"
+            name={["user", "phonenumber"]}
+            label="เบอร์โทรศัพท์"
+            rules={[
+              {
+                required: true,
+                message: "กรุณากรอกเบอร์โทรศัพท์",
+              },
+              {
+                pattern: /^0[6-9]{1}[0-9]{8}$/,
+                message: "กรุณากรอกเบอร์โทรศัพท์ให้ถูกต้อง",
+              },
+            ]}
+          >
+            <Input
+              className="border rounded-lg border-grey300 w-full h-11 px-4 py-2.5"
+              placeholder="กรุณากรอกเบอร์โทรศัพท์"
+            />
+          </Form.Item>
+
+          <Form.Item
+            className="mt-5"
+            name={["user", "email"]}
+            label="อีเมล"
+            rules={[
+              {
+                type: "email",
+                message: "กรุณากรอกอีเมลให้ถูกต้อง",
+              },
+              {
+                required: true,
+                message: "กรุณากรอกอีเมล",
+              },
+            ]}
+          >
+            <Input
+              className="border rounded-lg border-grey300 w-full h-11 px-4 py-2.5"
+              placeholder="กรุณากรอกอีเมล"
+            />
+          </Form.Item>
+
+          {/* Add password validation rules */}
+          <Form.Item
+            className="mt-5"
+            name={["user", "password"]}
+            label="รหัสผ่าน"
+            rules={[
+              {
+                required: true,
+                message: "กรุณากรอกรหัสผ่าน",
+              },
+              {
+                validator: (rule, value) => {
+                  if (
+                    !/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])/.test(
+                      value
+                    )
+                  ) {
+                    return Promise.reject(
+                      "กรุณากรอกรหัสผ่านให้ถูกต้อง"
+                    );
+                  }
+                  if (value.length < 8) {
+                    return Promise.reject(
+                      "รหัสผ่านต้องมีอย่างน้อย 8 ตัวอักษร"
+                    );
+                  }
+                  return Promise.resolve();
+                },
+              },
+            ]}
+          >
+            <Input.Password
+              className="border rounded-lg border-grey300 w-full h-11 px-4 py-2.5"
+              placeholder="กรุณากรอกรหัสผ่าน"
+            />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <div className="mt-[30px]">
+              <Checkbox>
+                ยอมรับ{" "}
+                <a href="#" className="underline text-blue950">
+                  ข้อตกลงและเงื่อนไข
+                </a>{" "}
+                และ{" "}
+                <a href="#" className="underline text-blue950">
+                  นโยบายความเป็นส่วนตัว
+                </a>
+              </Checkbox>
+            </div>
+
+            <Button
+              className="btn-primary w-full my-[30px]"
+              htmlType="submit"
+            >
+              ลงทะเบียน
+            </Button>
+            <div className="text-center">
+              <button className="btn-ghost">
+                กลับไปหน้าเข้าสู่ระบบ
+              </button>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
