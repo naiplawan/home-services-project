277749bed70686fa75299c247cffad8cbdefbf53
Benz <benzhomeme@gmail.com>
2023-09-06 18:21:59 +0700

    fix code

--- client/src/pages/AuthenticatedApp.jsx
+++ client/src/pages/AuthenticatedApp.jsx
@@ -6,6 +6,8 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import NotFoundPage from "./NotFoundPage";
+import LoginPage from './LoginPage'
+import RegisterPage from './RegisterPage'
 
 function AuthenticatedApp() {
   const loginRole = localStorage.getItem("role");
@@ -16,9 +18,13 @@ function AuthenticatedApp() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       ) : (
         <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
