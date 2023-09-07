351d8737207668373fce384114504893a7cdba39
Baitoey <you@example.com>
2023-09-07 21:17:15 +0700

    fix: edit admin dashboard

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 function AdminDashboard() {
+  
+  const navigate = useNavigate();
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
         <h1 className=" text-9xl"> Hello Admin !! </h1>
+
+        <button
+        className="btn primary"
+        onClick={() => navigate("/")}
+      >
+        <span className="text-lg ">👨‍🔧 GO HOME 👩‍🔧</span>
+
+      </button>
       </div>
     </>
   );
