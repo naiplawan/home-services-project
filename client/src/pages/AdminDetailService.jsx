09a537a3b8a7ebf77664a70206264ed794030e6a
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-09-18 17:21:27 +0700

    feat:add detail service page

---
+++ client/src/pages/AdminDetailService.jsx
@@ -0,0 +1,13 @@
+import AdminDetailServicePage from "../components/AdminServicePage/AdminDetailServicePage.jsx";
+import AdminSidebar from "../components/ServicePage/AdminSidebar.jsx";
+
+function AdminDetailService() {
+  return (
+    <div className="bg-bg h-screen  ">
+      <AdminSidebar />
+      <AdminDetailServicePage />
+    </div>
+  );
+}
+
+export default AdminDetailService;
