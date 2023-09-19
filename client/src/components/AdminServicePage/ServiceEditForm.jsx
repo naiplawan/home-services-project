e5fa55896030b66cf4f64e2f4bb5e35c53628015
Anurat Likitdachavong <130462497+JaqJaqq@users.noreply.github.com>
2023-09-19 20:41:26 +0700

    feat: add API PUT in service.js

--- client/src/components/AdminServicePage/ServiceEditForm.jsx
+++ client/src/components/AdminServicePage/ServiceEditForm.jsx
@@ -6,41 +6,40 @@ import {
     message,
     InputNumber,
     Image,
-    Button,
   } from "antd";
   import {
-    LoadingOutlined,
-    PlusOutlined,
     InboxOutlined,
   } from "@ant-design/icons";
   import { useNavigate } from "react-router-dom";
   // import  {useUtils}  from "../../hooks/utils";
   import { useState, useEffect } from "react";
   import axios from "axios";
+  import { useParams } from "react-router-dom";
   import { Droppable, Draggable, DragDropContext } from "react-beautiful-dnd";
   
   function ServiceEditForm() {
     const { Dragger } = Upload;
     const navigate = useNavigate();
+    const { serviceId } = useParams();
   
     const [data, setData] = useState([]);
     const [category, setCategory] = useState([]);
   
     const [selectedImage, setSelectedImage] = useState(null); //manage the selected image
-  
     const [selectedCategory, setSelectedCategory] = useState("เลือกหมวดหมู่");
-  
     const [fileList, setFileList] = useState([]);
+    
+    const [currentData, setCurrentData] = useState({
+      service_name: "",
+      category_id: "",
+      items: [],
+    });
   
     const handleFileChange = (file) => {
-  
-      console.log('file', file)
       const reader = new FileReader();
-  
       reader.onload = (e) => {
         setSelectedImage(e.target.result);
       };
-  
       reader.readAsDataURL(file);
       return false;
     };
@@ -49,44 +48,34 @@ import {
       setSelectedImage(null);
     };
   
-    console.log('ฟายลิส', fileList)
-  
     const handleSubmitService = async (values) => {
       try {
-        console.log("values", values);
-        console.log("selectedCategory", selectedCategory);
-        console.log("fileList:", fileList);
-  
         const selectedCategoryId = category.data.find(
           (category) => category.category_name === selectedCategory
         )?.category_id;
   
         const formData = new FormData();
         formData.append("service_name", values.service_name);
-  
         formData.append("category_id", selectedCategoryId);
-  
-        // formData.append("file", file)
-  
         formData.append("file", fileList[0]);
   
         values.items.forEach((item, index) => {
           formData.append(
             "items",
             JSON.stringify({
-              sub_service_name: item.name, // Change name to sub_service_name
+              sub_service_name: item.name, 
               unit: item.unit,
               price_per_unit: item.cost, // Change cost to price_per_unit
             })
           );
         });
   
-        for (const [key, value] of formData.entries()) {
-          console.log(`${key}: ${value}`);
-        }
+        // for (const [key, value] of formData.entries()) {
+        //   console.log(`${key}: ${value}`);
+        // }
   
-        const response = await axios.post(
-          "http://localhost:4000/service",
+        const response = await axios.put(
+          `http://localhost:4000/service/${serviceId}`,
           formData,
           {
             headers: { "Content-Type": "multipart/form-data" },
@@ -94,15 +83,15 @@ import {
         );
   
         if (response.status === 200) {
-          message.success("Successfully create service");
+          message.success("Successfully update service");
         } else {
-          message.error("Cannot create service");
+          message.error("Cannot update service");
         }
         navigate("/admin-service")
   
       } catch (error) {
         console.error(error);
-        message.error("Error creating service");
+        message.error("Error updateing service");
       }
     };
   
@@ -110,14 +99,14 @@ import {
   
     useEffect(() => {
       axios
-        .get("http://localhost:4000/category")
+        .get(`http://localhost:4000/category`)
         .then((response) => {
-          setData(response.data.data); // Store data in state
-          setCategory(response.data.data);
+          const currentService = response.data.data
+          setCurrentData(currentService);
           console.log(response.data);
         })
         .catch((error) => console.error("Error fetching data:", error));
-    }, []);
+    }, [serviceId]);
   
     console.log(data.data);
     console.log(category);
@@ -147,6 +136,7 @@ import {
           wrapperCol={{ span: 24 }}
           layout="horizontal"
           onFinish={handleSubmitService}
+          initialValues={currentData} // add initial values
         >
           <div className="bg-grey100 h-full pb-4% md:pb-0 md:pl-60">
             <div className="flex items-center h-20 px-10 justify-between border-b border-grey300 bg-white">
