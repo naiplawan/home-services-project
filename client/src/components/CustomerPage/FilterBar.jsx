4e6e74a78be06b048837896394c2d2a626a52e95
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-09-21 10:40:11 +0700

    feat: add filter bar UI / service list display can can fetching daata

---
+++ client/src/components/CustomerPage/FilterBar.jsx
@@ -0,0 +1,159 @@
+// FilterBar.jsx
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import InputPriceRange from "./InputPriceRange";
+
+function FilterBar({ onFilterChange }) {
+  const [searchText, setSearchText] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [priceRange, setPriceRange] = useState("");
+  const [sortBy, setSortBy] = useState("popular");
+
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+  //InputPriceRange
+  const [minFilter, setMinFilter] = useState(0);
+  const [maxFilter, setMaxFilter] = useState(2000);
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [isSearchBarExpanded, setSearchBarExpanded] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Fetch services from your API
+        const servicesResponse = await axios.get(
+          "http://localhost:4000/service"
+        );
+        const servicesData = servicesResponse.data.data;
+        setServices(servicesData);
+
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+        setLoading(false); // Set loading to false even on error
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const handleSortByChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
+  const handleFilterSubmit = () => {
+    const filters = {
+      searchText,
+      category: selectedCategory,
+      priceRange,
+      sortBy,
+    };
+    onFilterChange(filters);
+  };
+
+  return (
+    <div className="flex p-5 sticky top-0 z-[100] bg-white border-b-[1px] border-[#CCD0D7]">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="container mx-auto">
+          <div className="xl:px-[120px] flex row items-center space-x-4 justify-between">
+            <input
+              type="text"
+              placeholder="ค้นหาบริการ"
+              className={`px-4 py-2 border-grey300 border bg-white rounded-lg focus:outline-none focus:ring focus:border-blue-300 w-[318px] ${
+                isSearchBarExpanded ? "w-[500px]" : ""
+              }`}
+              value={searchText}
+              onChange={handleSearchChange}
+              onFocus={() => setSearchBarExpanded(true)}
+              onBlur={() => setSearchBarExpanded(false)}
+            />
+            <div className="w-[80px]"></div>
+            <div>
+              <p className="pl-[10px] text-[12px] text-[#646C80]">
+                หมวดหมู่บริการ
+              </p>
+              <select
+                onChange={handleCategoryChange}
+                value={selectedCategory}
+                className="px-2 bg-white text-[16px]"
+              >
+                <option value="All">บริการทั้งหมด</option>
+                {services &&
+                  services.data &&
+                  services.data.map((service) => (
+                    <option
+                      key={service.service_id}
+                      value={service.category.category_name}
+                    >
+                      {service.category.category_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="border-[#CCD0D7] border-l border-[1px] h-[44px] "></div>{" "}
+            <div>
+              <p className="pl-[10px] w-[120px] text-[12px] text-[#646C80]">
+                ราคา
+              </p>
+              <div className="relative inline-block">
+                <p
+                  className="cursor-pointer w-[150px] mb-1"
+                  onClick={() => setDropdownVisible(!isDropdownVisible)}
+                >
+                  {minFilter} - {maxFilter} ฿ ▾
+                </p>
+                {isDropdownVisible && (
+                  <div className="dropdown-content bg-white w-64 h-28 absolute right-[-50px] top-[50px] rounded-md shadow-lg">
+                    <div className="pt-[15px]">
+                      <InputPriceRange
+                        min={0}
+                        max={2000}
+                        minFilter={minFilter}
+                        setMinFilter={setMinFilter}
+                        maxFilter={maxFilter}
+                        setMaxFilter={setMaxFilter}
+                        onChange={({ min, max }) =>
+                          console.log(`min = ${min}, max = ${max}`)
+                        }
+                      />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="border-[#CCD0D7] border-l border-[1px] h-[44px] "></div>
+            <div>
+              <p className="pl-[10px] text-[12px] text-[#646C80]">
+                หมวดหมู่บริการ
+              </p>
+              <select
+                onChange={handleSortByChange}
+                value={sortBy}
+                className="px-2 bg-white"
+              >
+                <option value="popular">บริการแนะนำ</option>
+                <option value="alphabetical">ตามตัวอักษร</option>
+              </select>
+            </div>
+            <div className="w-[80px]"></div>
+            <button className="btn-primary" onClick={handleFilterSubmit}>
+              ค้นหา
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FilterBar;
--- client/src/components/CustomerPage/ServiceList.jsx
+++ client/src/components/CustomerPage/ServiceList.jsx
@@ -1,9 +1,191 @@
 // หน้าแรกที่มีการ fetching data มาแสดงรายการ 9 ช่อง และ filterbar
 // หน้านี้จะถูก render ที่ไฟล์ page / CustomerServiceListDisplay.jsx
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import image from "../../assets/CustomerPhoto/imageIndex.js";
+import "../../styles/App.css";
+
+import SellBlack from "../../assets/homepagePhoto/sellBlack.jsx";
+import FilterBar from "./FilterBar.jsx";
+
 function ServiceList() {
+  const navigate = useNavigate();
+  const initialFilters = {
+    searchText: "",
+    category: "All",
+    priceRange: "",
+    sortBy: "popular",
+  };
+
+  const [filters, setFilters] = useState(initialFilters);
+  const handleFilterChange = (newFilters) => {
+    // Update the filters state with the new filters
+    setFilters(newFilters);
+    // Perform filtering or other actions based on the new filters
+  };
+  const filterServices = async () => {
+    try {
+      // สร้างคำขอ GET ไปยัง API โดยใช้ค่า filters
+      const response = await axios.get("http://localhost:4000/service", {
+        params: {
+          searchText: filters.searchText,
+          category: filters.category,
+          priceRange: filters.priceRange,
+          sortBy: filters.sortBy,
+        },
+      });
+
+      const data = response.data.data;
+      setServices(data); // เซ็ตข้อมูลใหม่ใน services state
+      setLoading(false);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    filterServices(); // เรียกใช้ฟังก์ชันการกรองเมื่อ filters เปลี่ยนแปลง
+  }, [filters]);
+
+  //ราคา
+  function getMinPrice(subServicesArray) {
+    if (subServicesArray.length === 0) {
+      return 0;
+    }
+
+    let minPrice = subServicesArray[0].price_per_unit;
+
+    for (const subService of subServicesArray) {
+      if (subService.price_per_unit < minPrice) {
+        minPrice = subService.price_per_unit;
+      }
+    }
+
+    return minPrice;
+  }
+
+  function getMaxPrice(subServicesArray) {
+    if (subServicesArray.length === 0) {
+      return 0;
+    }
+
+    let maxPrice = subServicesArray[0].price_per_unit;
+
+    for (const subService of subServicesArray) {
+      if (subService.price_per_unit > maxPrice) {
+        maxPrice = subService.price_per_unit;
+      }
+    }
+
+    return maxPrice;
+  }
+
+  // *** user สามารถดูรายการ services ทั้งหมดได้ ***
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // สร้าง function ใหม่เพื่อทำการโหลดข้อมูลจาก API
+    async function fetchData() {
+      try {
+        const response = await axios.get("http://localhost:4000/service");
+        const data = response.data.data;
+        setServices(data); // นำข้อมูลที่ได้มาเก็บใน state
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
   return (
     <>
-      <h1>Service List Test</h1>
+      <div className="Header relative">
+        <img src={image.Header} alt="Test" className="inline" />
+        <div className="absolute top-0 h-[100%] left-0 right-0 z-10 text-center text-black bg-[#00195199]"></div>
+        <span className="absolute top-[64px]  left-0 right-0 z-10 text-center text-white ">
+          <h1 className="text-[32px]">บริการของเรา</h1>
+          <p>
+            ซ่อมเครื่องใช้ไฟฟ้า ซ่อมแอร์ ทำความสะอาดบ้าน และอื่น ๆ อีกมากมาย
+            <br />
+            โดยพนักงานแม่บ้าน และช่างมืออาชีพ
+          </p>
+        </span>
+      </div>
+      <FilterBar onFilterChange={handleFilterChange} />
+      <section className="services">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className=" xl:px-[159px]  flex flex-wrap justify-center items-center top-20 pb-20 h-full bg-[#f0f0f0]">
+            {services &&
+              services.data &&
+              services.data.map((service) => (
+                <div
+                  key={service.service_id}
+                  className="lg:mx-[37px] lg:w-[369px] mt-[48px] w-[25%] h-[35%] rounded-md overflow-hidden border border-[#CCD0D7] m-2"
+                >
+                  <div className="img-display">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={service.service_photo}
+                      alt={service.sub_service.name}
+                    />
+                  </div>
+                  <div className="p-2 md:p-5 bg-white min-h-full">
+                    <div className="bg-[#E7EEFF] text-center px-[10px] inline-block p-1 text-[#0E3FB0] rounded-lg">
+                      <p>{service.category.category_name}</p>
+                    </div>
+                    <h2 className="font-bold text-[20px] mt-3">
+                      {service.service_name}
+                    </h2>
+                    <div className="flex items-center">
+                      <SellBlack />
+                      <p className="ml-2 text-[#646C80] text-sm py-[10px]">
+                        {getMinPrice(service.sub_service) !==
+                        getMaxPrice(service.sub_service)
+                          ? `ค่าบริการประมาณ ${getMinPrice(
+                              service.sub_service
+                            )} - ${getMaxPrice(service.sub_service)} ฿`
+                          : `ค่าบริการประมาณ ${getMinPrice(
+                              service.sub_service
+                            )} ฿`}
+                      </p>
+                    </div>
+
+                    <div>
+                      <p
+                        className="link text-l font-semibold text-[#336DF2]"
+                        onClick={() =>
+                          navigate(`/checkout/${service.service_id}`)
+                        }
+                      >
+                        เลือกบริการ
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        )}
+      </section>
+      <footer className="h-[284px] bg-[#336DF2] text-center flex justify-center items-center">
+        <p className="text-[20px] text-white">
+          เพราะเราคือช่าง ผู้ให้บริการเรื่องบ้านอันดับ 1 แบบครบวงจร
+          โดยทีมช่างมืออาชีพมากกว่า 100 ทีม
+          <br />
+          สามารถตอบโจทย์ด้านการบริการเรื่องบ้านของคุณ และสร้าง
+          <br />
+          ความสะดวกสบายในการติดต่อกับทีมช่าง ได้ทุกที่ ทุกเวลา ตลอด 24 ชม.
+          <br />
+          มั่นใจ ช่างไม่ทิ้งงาน พร้อมรับประกันคุณภาพงาน
+        </p>
+        <img src="" />
+      </footer>
     </>
   );
 }
