cec16404650ce2a140615435d4b1b330ca251ad0
Baitoey <you@example.com>
2023-09-21 15:48:59 +0700

    fix: edit path on servist list

--- client/src/components/AdminServicePage/AdminServiceList.jsx
+++ client/src/components/AdminServicePage/AdminServiceList.jsx
@@ -216,7 +216,7 @@ function AdminServiceList() {
                                     alt="Edit"
                                     src={image.editIcon}
                                     onClick={() =>
-                                      navigate(`/service/edit/${serviceItem.service_id}`)
+                                      navigate('/admin-service-edit/:serviceId')
                                     }
                                   />
                                 </div>
