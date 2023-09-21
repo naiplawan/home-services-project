7a2ab626d9ba2fc10eefc931c9d8d45238773154
Anurat Likitdachavong <130462497+JaqJaqq@users.noreply.github.com>
2023-09-21 17:02:30 +0700

    feat: add order.map and style on RepairList and ListHistory components

--- client/src/components/CustomerPage/Title.jsx
+++ client/src/components/CustomerPage/Title.jsx
@@ -1,6 +1,6 @@
 function Title(props) {
     return (
-        <div className="flex justify-center items-center p-6 sticky top-0 z-[100] bg-blue600">
+        <div className="flex justify-center items-center p-6 bg-blue600">
             <h1 className="text-2xl text-white font-medium">{props.title}</h1>
         </div>
     );
--- server/apps/orderHistory.js
+++ server/apps/orderHistory.js
@@ -6,63 +6,20 @@ import supabase from "../utils/supabase.js";
 const orderHistoryRouter = Router();
 // orderHistoryRouter.use(protect);
 
-// API route to view order history by userId
-
-
-orderHistoryRouter.get('/', async (req, res) => {
+// API route to all order history
+orderHistoryRouter.get('/:id', async (req, res) => {
   try {
-    // ดึงข้อมูลจากตาราง users
-    const { data: users, error: usersError } = await supabase.from('users').select('*');
-    if (usersError) throw usersError;
+    const orderHistoryByUserId = req.params.id;
 
-    // ดึงข้อมูลจากตาราง service
-    const { data: services, error: servicesError } = await supabase.from('service').select('*');
-    if (servicesError) throw servicesError;
+    const { data: result, error } =  await supabase
+      .from("order_history")
+      .select(`*, users(*, service (*, sub_service (*, checkout_quantity (*, checkout (*)))))`)
+      .eq("user_id", orderHistoryByUserId);
 
-    // ดึงข้อมูลจากตาราง sub_service
-    const { data: subServices, error: subServicesError } = await supabase.from('sub_service').select('*');
-    if (subServicesError) throw subServicesError;
-
-    // ดึงข้อมูลจากตาราง checkout_quantity
-    const { data: checkoutQuantities, error: checkoutQuantitiesError } = await supabase.from('checkout_quantity').select('*');
-    if (checkoutQuantitiesError) throw checkoutQuantitiesError;
-
-    // ดึงข้อมูลจากตาราง checkout
-    const { data: checkouts, error: checkoutsError } = await supabase.from('checkout').select('*');
-    if (checkoutsError) throw checkoutsError;
-
-    // ดึงข้อมูลจากตาราง order_history
-    const { data: orderHistories, error: orderHistoriesError } = await supabase.from('order_history').select('*');
-    if (orderHistoriesError) throw orderHistoriesError;
-
-    // รวมข้อมูลเข้าด้วยกัน
-    const combinedData = users.map((user) => {
-      // รวมข้อมูลบริการของแต่ละผู้ใช้
-      user.services = services.filter((service) => service.user_id === user.user_id);
-
-      // รวมข้อมูลบริการย่อยของแต่ละบริการ
-      user.services.forEach((service) => {
-        service.sub_services = subServices.filter((subService) => subService.service_id === service.service_id);
+      return res.status(200).json({
+        result,
       });
 
-      // รวมข้อมูลการสั่งซื้อและปริมาณการสั่งซื้อของแต่ละบริการ
-      user.services.forEach((service) => {
-        service.checkouts = checkouts.filter((checkout) => checkout.service_id === service.service_id);
-
-        service.checkouts.forEach((checkout) => {
-          checkout.checkout_quantities = checkoutQuantities.filter(
-            (cq) => cq.checkout_id === checkout.checkout_id
-          );
-        });
-      });
-
-      // รวมข้อมูลประวัติการสั่งซื้อของแต่ละผู้ใช้
-      user.order_histories = orderHistories.filter((oh) => oh.user_id === user.user_id);
-
-      return user;
-    });
-
-    res.json(combinedData);
   } catch (error) {
     res.status(500).json({ error: 'เกิดข้อผิดพลาดในการดึงข้อมูลและรวมข้อมูล' });
   }
