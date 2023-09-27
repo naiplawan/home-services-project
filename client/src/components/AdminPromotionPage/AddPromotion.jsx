f854c38dde8a6acf201cd7c46a31db356ef21b61
Baitoey <you@example.com>
2023-09-27 09:53:04 +0700

    feat: add suffix

--- client/src/components/AdminPromotionPage/AddPromotion.jsx
+++ client/src/components/AdminPromotionPage/AddPromotion.jsx
@@ -30,7 +30,7 @@ function AddPromotionForm() {
         "YYYY-MM-DD"
       );
       const formattedExpiryTime = moment(values.promotion_expiry_time).format(
-        "HH:mm:ss"
+        "HH:mm"
       );
 
       formData.append("promotion_expiry_date", formattedExpiryDate);
@@ -121,7 +121,8 @@ function AddPromotionForm() {
             ]}
           >
             <Radio.Group>
-              <div style={{ marginBottom: "8px" }}>
+              <div 
+              className="flex flex-row">
                 <Form.Item
                   name="promotion_types"
                   valuePropName="checked"
@@ -159,14 +160,16 @@ function AddPromotionForm() {
                           },
                         ]}
                       >
-                        <Input style={{ width: "50%" }} />
+                        <Input style={{ width: "50%" }} 
+                        suffix="฿"
+                        disabled={getFieldValue("promotion_types") !== "fixed"}/>
                       </Form.Item>
                     ) : null;
                   }}
                 </Form.Item>
               </div>
 
-              <div>
+              <div  className="flex flex-row">
                 <Form.Item
                   name="promotion_types"
                   valuePropName="checked"
@@ -204,7 +207,9 @@ function AddPromotionForm() {
                           },
                         ]}
                       >
-                        <Input style={{ width: "50%" }} />
+                        <Input style={{ width: "50%" }}
+                        suffix="%" 
+                        disabled={getFieldValue("promotion_types") !== "percent"}/>
                       </Form.Item>
                     ) : null;
                   }}
@@ -327,7 +332,8 @@ function AddPromotionForm() {
               },
             ]}
           >
-            <Input style={{ width: "50%" }} />
+            <Input style={{ width: "50%" }} 
+            suffix="ครั้ง"/>
           </Form.Item>
 
           <Form.Item
@@ -366,7 +372,7 @@ function AddPromotionForm() {
                   ]}
                   noStyle
                 >
-                  <TimePicker format="HH:mm:ss" />
+                  <TimePicker format="HH:mm" />
                 </Form.Item>
               </Col>
             </Row>
